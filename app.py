4839506efd2a752076a9b3a28ca3b995a34a543b
Casper <capsulechew@gmail.com>
2026-07-12 12:26:39 +0800

    .

--- app.py
+++ app.py
@@ -338,7 +338,7 @@ for i, model in enumerate(models):
 
         pred_delta = pred
         if st.session_state.get(model.name) is not None:
-            pred_delta = (pred - st.session_state.get(model.name))
+            pred_delta = (pred - st.session_state.get(model.name)).round(3)
 
         st.session_state[model.name] = pred
 
